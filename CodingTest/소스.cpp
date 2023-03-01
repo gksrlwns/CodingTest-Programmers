30da098890ab807f9ee00955022ed6cf4c2c4e62
gksrlwns <83814481+gksrlwns@users.noreply.github.com>
2023-03-01 23:57:06 +0900

    .

---
+++ CodingTest/소스.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<string> s1, vector<string> s2) {
+    int answer = 0;
+	for each (object var in collection_to_loop)
+	{
+
+	}
+    return answer;
+}
+int main()
+{
+
+}
