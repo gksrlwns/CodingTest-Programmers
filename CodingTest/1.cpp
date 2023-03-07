7621b2d6ad77bab77b2c11bf7a8da55203366ea8
gksrlwns <83814481+gksrlwns@users.noreply.github.com>
2023-03-07 19:33:27 +0900

    1

--- CodingTest/1.cpp
+++ CodingTest/1.cpp
@@ -3,6 +3,183 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+int solutionAdd(int n);
+int main()
+{
+    vector<int> v = { 1,2,3,4,5 };
+    //cout << solutionCompare()
+}
+//개미군단
+int solutionAnt(int hp) {
+    int answer = 0;
+    while (1)
+    {
+        if (hp >= 5)
+        {
+            answer = hp / 5;
+            hp %= 5;
+        }
+        else if (hp >= 3)
+        {
+            answer += hp / 3;
+            hp %= 3;
+        }
+        else
+        {
+            answer += hp;
+            break;
+        }
+    }
+    return answer;
+}
+
+//스트링 속 자연수
+int solutionNum(string my_string) {
+    int answer = 0;
+    
+    return answer;
+}
+
+//문자여 비교
+int solutionCompare(vector<string> s1, vector<string> s2) {
+    int answer = 0;
+    for(auto a : s1)
+        for (auto b : s2)
+        {
+            if (b.compare(a) == 0)
+                answer++;
+        }
+    return answer;
+}
+
+// 자릿수더하기
+int solutionAdd(int n) {
+    string toString = to_string(n);
+    cout << toString << endl;
+    int answer = 0;
+    for (int a : toString)
+    {
+        cout << a << endl;
+        answer += a;
+    }
+    /*int answer = 0;
+    while (n > 0)
+    {
+        answer += n % 10;
+        n /= 10;
+    }
+    return answer;*/
+    return answer;
+}
+
+//편지
+int solutionLetter(string message) {
+    int answer = message.length() * 2;
+    return answer;
+}
+//짝수 싫어요
+vector<int> solutionOddNum(int n) {
+    vector<int> answer;
+    for (int i = 1; i <= n; i ++) if (i % 2 == 1) answer.push_back(i);
+    return answer;
+}
+
+
+//삼각형 변 2점? 3점?
+int solutionSides(vector<int> sides) {
+    sort(sides.begin(), sides.end());
+    return sides[2] < sides[0] + sides[1] ? 1 : 2;
+}
+
+//중앙값 3점
+int solutionCenter(vector<int> array) {
+    sort(array.begin(), array.end());
+    int i = array.size() / 2;
+    return array[i];
+}
+
+//최댓값 만들기 다르게풀기
+int solutionMax(vector<int> numbers) {
+    int max = 0;
+    int j = 0;
+    int max2 = 0;
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+            j = i;
+        }
+    }
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        if (i != j && numbers[i] <= max) max2 = numbers[i];
+    }
+    /*sort(numbers.begin(), numbers.end()); \
+     answer = numbers[numbers.size() - 2] * numbers[numbers.size() - 1];*/
+    
+    return max*max2;
+}
+
+//최댓값 만들기 1
+//int solutionMax(vector<int> numbers) {
+//    int max = 0;
+//    int max2 = 0;
+//    for (auto a : numbers)
+//    {
+//        if (a > max)
+//        {
+//            max = a;
+//            cout << max << endl;
+//        }
+//    }
+//    
+//    for (auto a : numbers)
+//    {
+//        if (a <= max && a > max2)
+//        {
+//            max2 = a;
+//            cout << max2 << endl;
+//        }
+//    }
+//    cout << "max =" << max << ", max2 =" << max2 << endl;
+//    return max * max2;
+//}
+
+//10점이요 옷가게할인
+//int solution(int price) {
+//    float discount = 0;
+//    discount = price >= 500000 ? 0.2 : (price >= 300000 ? 0.1 : (price >= 100000 ? 0.05 : 0));
+//    return price - price*discount;
+//}
+
+//2
+int solution(vector<int> dot) {
+    int answer = 0;
+    if (dot[0] * dot[1] > 0)
+    {
+        if (dot[0] > 0) answer = 1;
+        else answer = 3;
+    }
+    else
+    {
+        if (dot[0] > 0) answer = 4;
+        else answer = 2;
+    }
+    //return dot[0] > 0 ? (dot[1] > 0 ? 1 : 4) : (dot[1] < 0 ? 3 : 2);
+    return answer;
+}
+
+int solution(vector<int> array, int n) {
+    int answer = 0;
+    //answer = count(array.begin(), array.end(), n);
+    for (auto a : array)
+    {
+        if (a == n)
+            answer++;
+    }
+    return answer;
+}
 string solution(string my_string, string letter) {
     string answer = "";
     for (int i = 0; i < my_string.size(); i++)
@@ -23,7 +200,7 @@ int solution(int n) {
     return answer;
 }
 
-vector<int> solution(vector<int> num_list) {
+vector<int> solution1(vector<int> num_list) {
     vector<int> answer = { 0,0 };
     for (int i = 0; i < num_list.size(); i++)
     {
@@ -34,7 +211,7 @@ vector<int> solution(vector<int> num_list) {
     }
     return answer;
 }
-vector<int> solution(vector<int> num_list) {
+vector<int> solution2(vector<int> num_list) {
     vector<int> answer(2, 0);
     for (int num : num_list) {
         answer[num % 2]++;
@@ -122,7 +299,3 @@ vector<int> solution(vector<int> num_list) {
 //    }
 //    return answer / numbers.size();
 //}
-int main()
-{
-    cout << solution(20);
-}
