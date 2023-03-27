0583f4f302c19145ba736961997ed89cde73691f
gksrlwns <83814481+gksrlwns@users.noreply.github.com>
2023-03-27 22:31:44 +0900

    /..

--- CodingTest/1.cpp
+++ CodingTest/1.cpp
@@ -21,8 +21,19 @@ int main()
     cout << solutionAlien(v, v1);
     
 }
-vector<int> solution(vector<string> keyinput, vector<int> board) {
+vector<int> solutionCharacter(vector<string> keyinput, vector<int> board) {
     vector<int> answer;
+    for (auto a : keyinput)
+    {
+        if (a == "up") answer[1]++;
+        else if (a == "down") answer[1]--;
+        else if (a == "left") answer[0]--;
+        else if (a == "right") answer[0]--;
+    }
+    if (abs(answer[0]) > board[0] / 2 && answer[0] > 0) answer[0] = board[0] / 2;
+    else if (abs(answer[0]) > board[0] / 2 && answer[0] < 0) answer[0] = board[0] / 2 * -1;
+    if (abs(answer[1]) > board[1] / 2 && answer[1] > 0) answer[1] = board[1] / 2;
+    else if (abs(answer[1]) > board[1] / 2 && answer[1] < 0) answer[1] = board[1] / 2 * -1;
     return answer;
 }
 
---
+++ 캐릭터의좌표.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+vector<int> solution(vector<string> keyinput, vector<int> board) {
+    vector<int> answer;
+    for(auto a : keyinput)
+	{
+		if(a == "up") answer[1]++;
+		else if(a == "down") answer[1]--;
+		else if(a == "left") answer[0]--;
+		else if(a == "right") answer[0]--;
+	 } 
+	 if(abs(answer[0]) > board[0]/2 && answer[0] > 0) answer[0] = board[0]/2;
+	 else if(abs(answer[0]) > board[0]/2 && answer[0] < 0) answer[0] = board[0]/2 *-1;
+	 if(abs(answer[1]) > board[1]/2 && answer[1] > 0) answer[1] = board[1]/2;
+	 else ifabs(answer[1]) > board[1]/2 && answer[1] < 0) answer[1] = board[1]/2 *-1;
+    return answer;
+}
