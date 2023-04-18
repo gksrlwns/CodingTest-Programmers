cab9d544702556b27dcb61d456196bf48cadb7a8
gksrlwns <83814481+gksrlwns@users.noreply.github.com>
2023-04-18 18:29:01 +0900

    안전지대

--- CodingTest/1.cpp
+++ CodingTest/1.cpp
@@ -21,8 +21,8 @@ int main()
     vector<int> v2 = { 1, 2, 3, 4, 5, 6 };
     vector<int> v3 = { 1,2};
     vector<string> s1 = { "19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2" };
-    vector<vector<int>> vvi = { {0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 1, 0, 0},{0, 0, 0, 0, 0} };
-    solutionSafeZone(vvi);
+    vector<vector<int>> vvi = { {0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 1, 1, 0},{0, 0, 0, 0, 0} };
+    cout << solutionSafeZone(vvi);
     
     //cout << Divisor3(99);
     //vector<int> v1 = solutionCharacter(s, v);
@@ -31,16 +31,49 @@ int main()
 }
 int solutionSafeZone(vector<vector<int>> board) {
     int answer = 0;
+    vector<vector<int>> x;
     for (int i = 0; i < board.size(); i++)
     {
         for (int j = 0; j < board[i].size(); j++)
         {
             if (board[i][j] == 1)
             {
+                vector<int> temp;
                 cout << "i = " << i << ", j = " << j << endl;
+                temp.push_back(i);
+                temp.push_back(j);
+                x.push_back(temp);
             }
         }
     }
+    for (auto a : x)
+    {
+        cout << "[";
+        for (auto b : a)
+            cout << b << ",";
+        cout << "]";
+    }
+    
+    for (auto a : x)
+    {
+        for (int i = a[0] - 1; i <= a[0] + 1; i++)
+        {
+            if (i == -1 || i == board.size())
+                continue;
+            for (int j = a[1] - 1; j <= a[1] + 1; j++)
+            {
+                if (j == -1 || j == board.size())
+                    continue;
+                if(board[i][j] == 0)
+                    board[i][j] = 1;
+            }
+        }
+    }
+    
+    for (auto a : board)
+        for (auto b : a)
+            if (b == 0) answer++;
+
     return answer;
 }
 
--- 안전지대.cpp
+++ 안전지대.cpp
@@ -2,8 +2,43 @@
 #include <vector>
 
 using namespace std;
-
+//4Á¡ 
 int solution(vector<vector<int>> board) {
     int answer = 0;
+    vector<vector<int>> x;
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[i].size(); j++)
+        {
+            if (board[i][j] == 1)
+            {
+                vector<int> temp;
+                temp.push_back(i);
+                temp.push_back(j);
+                x.push_back(temp);
+            }
+        }
+	}	
+    
+    for (auto a : x)
+    {
+        for (int i = a[0] - 1; i <= a[0] + 1; i++)
+        {
+            if (i == -1 || i == board.size())
+                continue;
+            for (int j = a[1] - 1; j <= a[1] + 1; j++)
+            {
+                if (j == -1 || j == board.size())
+                    continue;
+                if(board[i][j] == 0)
+                    board[i][j] = 1;
+            }
+        }
+    }
+    
+    for (auto a : board)
+        for (auto b : a)
+            if (b == 0) answer++;
+
     return answer;
 }
